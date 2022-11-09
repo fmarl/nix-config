734e363af4633cca56c375ba8adee6dd81b8891a
Florian Büstgens <fb@fx-ttr.de>
2022-11-09 06:16:38 +0100

    init commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "My Home Manager Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: rec {
+    legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+      import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      }
+    );
+
+    nixosConfiguration = {
+      workstation = nixpkgs.lib.nixosSystem {
+        pkgs = legacyPackages.x86_64-linux;
+        specialArgs = { inherit inputs; };
+        modules = [ ./nixos/configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      "florian@workstation" = home-manager.lib.homeManagerConfiguration {
+        pkgs = legacyPackages.x86_64-linux;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./nixos/home.nix ];
+      };
+    };
+  };
+}
