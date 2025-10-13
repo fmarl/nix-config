a721a2d0b0fbb1ce87cd1f8d592d0bab24f7837c
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-10-13 08:24:27 +0200

    Update emacs cfg

--- modules/home-manager/programs/emacs.nix
+++ modules/home-manager/programs/emacs.nix
@@ -13,38 +13,53 @@ in
   options.modules.emacs.enable = mkEnableOption "Install and configure emacs";
 
   config = mkIf cfg.enable {
-    home.file = {
-      ".emacs.d" = {
-        source = ./emacs;
-        recursive = true;
-      };
-    };
+    # home.file = {
+    #   ".emacs.d" = {
+    #     source = ./emacs;
+    #     recursive = true;
+    #   };
+    # };
 
     services.emacs.enable = true;
-    programs.emacs = {
-      enable = true;
+programs.emacs = {
+  enable = true;
 
-      extraPackages = (
-        epkgs:
-        [
-          epkgs.use-package
-        ]
-        ++ (with epkgs.melpaStablePackages; [
-          zenburn-theme
-          smart-mode-line
-          smart-mode-line-powerline-theme
-          smex
-          dashboard
-          markdown-mode
-          ace-window
-          ace-jump-mode
-          which-key
-          direnv
-          beacon
-          posframe
-          magit
-        ])
-      );
-    };
+  extraPackages = epkgs: with epkgs; [
+    use-package
+    zenburn-theme
+    smart-mode-line
+    smart-mode-line-powerline-theme
+    smex
+    ace-window
+    ace-jump-mode
+    which-key
+    direnv
+    beacon
+    posframe
+    magit
+    projectile
+    treemacs
+    treemacs-projectile
+    lsp-mode
+    lsp-ui
+    lsp-ivy
+    dap-mode
+    rustic
+    rust-mode
+    zig-mode
+    ccls
+    clang-format
+    corfu
+    cape
+    kind-icon
+    yasnippet
+    yasnippet-snippets
+    markdown-mode
+    geiser
+    paredit
+    rainbow-delimiters
+  ];
+};
+
   };
 }
