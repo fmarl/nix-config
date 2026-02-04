84677cf46b3f416953c86a0a8ba1771fa50a29bc
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2026-02-04 21:09:26 +0100

    Enable unprivileged_userns_clone to allow chromium sandboxes

--- modules/nixos/global/security.nix
+++ modules/nixos/global/security.nix
@@ -88,6 +88,7 @@ with lib;
       "net.ipv4.conf.default.accept_source_route" = mkDefault 0;
       "kernel.sysrq" = mkDefault 0;
       "kernel.unprivileged_bpf_disabled" = mkDefault 1;
+      "kernel.unprivileged_userns_clone" = mkDefault 1;
       "kernel.core_uses_pid" = mkDefault 1;
       "dev.tty.ldisc_autoload" = mkDefault 0;
 
